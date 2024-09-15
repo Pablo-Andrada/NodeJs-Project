00580699200377d77e4e4a573696c180ac24abad
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-09-15 18:58:39 -0300

    Server configurado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import ejs from 'ejs';
+import { dirname,join } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-app.get('/', (req,res) => res.send('Hello world'))
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+
+app.set('views',join(__dirname , 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => res.render('index'))
 
 
 app.listen(3000);
