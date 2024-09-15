d9bfe3e38c0d9efcacacb61b554ca3af0f7e6e65
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-09-15 14:01:17 -0300

    Inicializando server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import express from 'express';
+
+const app = express();
+app.get('/', (req,res) => res.send('Hello world'))
+
+
+app.listen(3000);
+
+console.log('Server is listening on port: ',3000);
+
