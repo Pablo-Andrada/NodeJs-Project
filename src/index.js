3cc5932dc476be778e499620077ea720f2d000c9
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-09-21 08:29:21 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views',join(__dirname , 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req,res) => res.render('index'))
+
 
 
 app.listen(3000);
