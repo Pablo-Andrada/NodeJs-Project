4bcdc3c991eb3251787403c1b14cb9b9458eca0f
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-09-23 21:50:36 -0300

    empezando con los estilos

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('views',join(__dirname , 'views'))
 app.set('view engine', 'ejs')
+
 app.use(indexRoutes);
 
-
+app.use(express.static(join(__dirname, 'public')));
 
 app.listen(3000);
 
